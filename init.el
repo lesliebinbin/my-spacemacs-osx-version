9f3f66cce136c9557a0e90eba33cf7e60a42e0c4
Leslie Binbin <lesliebinbin19900129@gmail.com>
2020-07-04 02:24:09 +1000

    hahaha

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 ;;
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
-
+;; hahahaha
 (doom! :input
        ;;chinese
        ;;japanese
