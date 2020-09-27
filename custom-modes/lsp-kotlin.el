3dfebb26c0b418544d8098d2eeb3b535270f77b0
lesliebinbin <lesliebinbin19900129@gmail.com>
2020-09-27 18:45:52 +1000

    adding ob-cypher

--- custom-modes/lsp-kotlin.el
+++
@@ -1,8 +0,0 @@
-(lsp-register-client
- (make-lsp--client
-  :new-connection
-  (lsp-stdio-connection '("/home/lesliebinbin/Desktop/code-from-github/kotlin-language-server/server/build/install/server/bin/kotlin-language-server"))
-  :major-modes '(kotlin-mode)
-  :server-id 'kotlin-language-server
-  ))
-(provide 'lsp-kotlin)
